20eca2aa65a074f206023e173369a0241b0b3d44
coffee.ewa@gmail.com <coffee.ewa@gmail.com>
2022-12-10 11:54:48 +0800

    add mongoose todoSchema

---
+++ models/todo.js
@@ -0,0 +1,19 @@
+//載入mongoose
+const mongoose = require('mongoose')
+
+//載入mongoose的Schema架構
+const Schema = mongoose.Schema
+
+//每一筆todoSchema都是一筆新的Schema
+const todoSchema = new Schema({
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 欄位=必填欄位
+  },
+  done: {
+    type: Boolean
+  }
+})
+
+//匯出模組
+module.exports = mongoose.model('Todo', todoSchema)
